f4c9ce69c08ca5c6dfb08a5d5383b26bb9564862
Mirim_Lee <leemirim9654@gmail.com>
2018-07-10 21:30:21 +0900

    Search users by username

--- nomadgram3/users/urls.py
+++ nomadgram3/users/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('explore/', views.Explore.as_view(), name='explore'),
     path('<int:user_id>/follow/', views.FollowUser.as_view(), name='follow_user'),
     path('<int:user_id>/unfollow/', views.UnFollowUser.as_view(), name='unfollow_user'),
+    path('search/', views.Search.as_view(), name='search'),
     path('<str:username>/', views.Userprofile.as_view(), name='user_profile'),
     path('<str:username>/followers/', views.UserFollowers.as_view(), name='user_followers'),
     path('<str:username>/following/', views.UserFollowing.as_view(), name='user_following'),
+    
 ]
